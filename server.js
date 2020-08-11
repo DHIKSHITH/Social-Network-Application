4b74c5e161351cba08120e047893a340e3d61643
DHIKSHITH <44042177+DHIKSHITH@users.noreply.github.com>
2020-08-11 19:38:59 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config({ path: "./config.env" });
 
@@ -19,7 +20,14 @@ mongoose
   })
   .then(() => console.log("connected"));
 
-const port = 5000 || process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("app running");
 });
